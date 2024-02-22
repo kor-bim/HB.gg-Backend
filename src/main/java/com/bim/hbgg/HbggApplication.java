a5116699401837232546ef1c11671eed5ce9119d
Yoon Han Bin <69673803+kor-bim@users.noreply.github.com>
2024-02-22 17:12:16 +0900

    Initial Project

---
+++ src/main/java/com/bim/hbgg/HbggApplication.java
@@ -0,0 +1,13 @@
+package com.bim.hbgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbggApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bim/hbgg/HbggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bim.hbgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
