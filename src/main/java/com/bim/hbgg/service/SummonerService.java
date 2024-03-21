768fc7168198ccec4ca3c82ac09b15c4372e93c2
Yoon Han Bin <69673803+kor-bim@users.noreply.github.com>
2024-03-21 10:20:00 +0900

    ⚡️ feat: 소환사 생성 및 조회 로직 개선
    소환사 정보와 리그 정보를 가져오는 로직을 개선하여 API 응답 시간과 데이터 정확성을 향상시켰습니다. RiotApiService를 통해 필요한 데이터를 가져오고, 이를 기반으로 Summoner와 League 객체를 생성하거나 업데이트합니다.

--- src/main/java/com/bim/hbgg/service/SummonerService.java
+++ src/main/java/com/bim/hbgg/service/SummonerService.java
@@ -1,62 +1,87 @@
 package com.bim.hbgg.service;
 
+import com.bim.hbgg.domain.League;
 import com.bim.hbgg.domain.Summoner;
-import com.bim.hbgg.dto.AccountDto;
+import com.bim.hbgg.dto.AccountDTO;
+import com.bim.hbgg.dto.LeagueEntryDTO;
+import com.bim.hbgg.dto.SummonerDTO;
+import com.bim.hbgg.dto.SummonerLeagueDto;
+import com.bim.hbgg.repository.LeagueRepository;
 import com.bim.hbgg.repository.SummonerRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class SummonerService {
-
-    private static final Logger logger = LoggerFactory.getLogger(SummonerService.class);
-
+    private static final Logger logger = LoggerFactory.getLogger(RiotApiService.class);
     private final SummonerRepository summonerRepository;
-    private final RestTemplate restTemplate;
-
-    @Value("${riot.api.key}")
-    private String apiKey;
+    private final LeagueRepository leagueRepository;
+    private final RiotApiService riotApiService;
 
     @Autowired
-    public SummonerService(SummonerRepository summonerRepository, RestTemplate restTemplate) {
+    public SummonerService(SummonerRepository summonerRepository, LeagueRepository leagueRepository, RiotApiService riotApiService) {
         this.summonerRepository = summonerRepository;
-        this.restTemplate = restTemplate;
+        this.leagueRepository = leagueRepository;
+        this.riotApiService = riotApiService;
     }
 
     @Transactional
-    public Summoner getOrCreateSummoner(String gameName, String tagLine) {
-        Optional<Summoner> existingSummoner = summonerRepository.findByGameNameAndTagline(gameName, tagLine);
-        return existingSummoner.orElseGet(() -> {
-            AccountDto accountDto = fetchAccountFromRiotApi(gameName, tagLine);
+    public SummonerLeagueDto getOrCreateSummoner(String summonerName, String tagLine) {
+        Optional<Summoner> existingSummoner = summonerRepository.findByTagLineAndSummonerName(tagLine, summonerName);
+        SummonerLeagueDto summonerLeagueDto = new SummonerLeagueDto();
+        existingSummoner.orElseGet(() -> {
+            AccountDTO accountDto = riotApiService.fetchAccountFromRiotApi(summonerName, tagLine);
+
             Summoner newSummoner = new Summoner();
-            newSummoner.setPuuid(accountDto.getPuuid());
-            newSummoner.setGameName(accountDto.getGameName());
-            newSummoner.setTagline(accountDto.getTagLine());
-            summonerRepository.save(newSummoner);
+
+            if (!accountDto.getPuuid().isEmpty()) {
+                SummonerDTO summonerDto = riotApiService.fetchSummonerFromRiotApi(accountDto.getPuuid());
+
+                newSummoner.setSummonerId(summonerDto.getId());
+                newSummoner.setAccountId(summonerDto.getAccountId());
+                newSummoner.setPuuid(accountDto.getPuuid());
+                newSummoner.setProfileIconId(summonerDto.getProfileIconId());
+                newSummoner.setSummonerName(accountDto.getGameName());
+                newSummoner.setTagLine(accountDto.getTagLine());
+                newSummoner.setInternalName(summonerDto.getName());
+                newSummoner.setSummonerLevel(summonerDto.getSummonerLevel());
+                newSummoner.setRevisionDate(summonerDto.getRevisionDate());
+
+                summonerLeagueDto.setSummoner(newSummoner);
+                summonerRepository.save(newSummoner);
+
+                if (!summonerDto.getId().isEmpty()) {
+
+                    List<LeagueEntryDTO> leagueDtos = riotApiService.fetchLeagueFromRiotApi(summonerDto.getId());
+
+                    for (LeagueEntryDTO leagueDto : leagueDtos) {
+                        League newLeague = new League();
+                        newLeague.setId(leagueDto.getLeagueId());
+                        newLeague.setSummonerId(leagueDto.getSummonerId());
+                        newLeague.setQueueType(leagueDto.getQueueType());
+                        newLeague.setTier(leagueDto.getTier());
+                        newLeague.setRank(leagueDto.getRank());
+                        newLeague.setLeaguePoint(leagueDto.getLeaguePoints());
+                        newLeague.setWin(leagueDto.getWins());
+                        newLeague.setLose(leagueDto.getLosses());
+
+                        summonerLeagueDto.setLeagueEntry(newLeague);
+
+                        leagueRepository.save(newLeague);
+                    }
+                }
+            }
+
+
             return newSummoner;
         });
-    }
 
-    private AccountDto fetchAccountFromRiotApi(String gameName, String tagLine) {
-        String url = String.format("https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s", gameName, tagLine);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Riot-Token", apiKey);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        logger.debug("Requesting Riot API for gameName: {}, tagLine: {}", gameName, tagLine);
-        ResponseEntity<AccountDto> response = restTemplate.exchange(url, HttpMethod.GET, entity, AccountDto.class);
-        logger.debug("Response from Riot API: {}", response);
-
-        return response.getBody();
+        return summonerLeagueDto;
     }
 }
