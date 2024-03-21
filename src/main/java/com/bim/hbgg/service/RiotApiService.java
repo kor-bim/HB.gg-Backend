5af1703362d56acc4c93ac2604f44924aadd8391
Yoon Han Bin <69673803+kor-bim@users.noreply.github.com>
2024-03-21 10:19:49 +0900

    ✨ feat: Riot API 호출 서비스 구현
    Riot API를 호출하는 서비스를 구현하여 외부 시스템과의 통신을 관리합니다. AccountDTO, SummonerDTO, LeagueEntryDTO를 이용하여 각각 계정, 소환사, 리그 정보를 가져올 수 있는 메소드를 구현했습니다.

---
+++ src/main/java/com/bim/hbgg/service/RiotApiService.java
@@ -0,0 +1,74 @@
+package com.bim.hbgg.service;
+
+import com.bim.hbgg.dto.AccountDTO;
+import com.bim.hbgg.dto.LeagueEntryDTO;
+import com.bim.hbgg.dto.SummonerDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class RiotApiService {
+    private static final Logger logger = LoggerFactory.getLogger(RiotApiService.class);
+
+    private final RestTemplate restTemplate;
+
+    @Value("${riot.api.key}")
+    private String apiKey;
+
+    @Autowired
+    public RiotApiService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+     public AccountDTO fetchAccountFromRiotApi(String gameName, String tagLine) {
+        String url = String.format("https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s", gameName, tagLine);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Riot-Token", apiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        logger.debug("Requesting Riot Account API for gameName: {}, tagLine: {}", gameName, tagLine);
+        ResponseEntity<AccountDTO> response = restTemplate.exchange(url, HttpMethod.GET, entity, AccountDTO.class);
+        logger.debug("Response from Riot Account API: {}", response);
+
+        return response.getBody();
+    }
+
+    public SummonerDTO fetchSummonerFromRiotApi(String puuid) {
+        String url = String.format("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/%s", puuid);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Riot-Token", apiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        logger.debug("Requesting Riot Summoner API for puuid: {}", puuid);
+        ResponseEntity<SummonerDTO> response = restTemplate.exchange(url, HttpMethod.GET, entity, SummonerDTO.class);
+        logger.debug("Response from Riot Summoner API: {}", response);
+
+        return response.getBody();
+    }
+
+    public List<LeagueEntryDTO> fetchLeagueFromRiotApi(String summonerId) {
+        String url = String.format("https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/%s", summonerId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Riot-Token", apiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        logger.debug("Requesting Riot League API for summonerId: {}", summonerId);
+
+        ResponseEntity<List<LeagueEntryDTO>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<LeagueEntryDTO>>() {});
+        logger.debug("Response from Riot League API: {}", response);
+
+        return response.getBody();
+    }
+}
